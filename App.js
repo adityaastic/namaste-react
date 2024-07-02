96aacc48373c7e51c524cc40f2cfa83c4230f2d4
adityaastic <adityaastic@gmail.com>
2024-07-02 12:44:04 +0530

    END of INCEPTION

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-        const heading = React.createElement(
-            "h1",
-             {id:"heading" , xyz:"abc"},
-              "Hello World From React!");
+/* <div id="parent">
+     <div id="child">
+        <h1>i'm h1 tag</h1>
+        <h2>i'm h2 tag</h2>
+    </div>
+<div id="child2">
+        <h1>i'm h1 tag</h1>
+        <h2>i'm h2 tag</h2>
+    </div>
+
+</div> */
+
+// ReactElement(Object) => HTML(Browser Understandings)
 
 
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-
-        root.render(heading);
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+]);
+    
+    //JSX
+    console.log(parent); // object
+    const root = ReactDOM.createRoot(document.getElementById("header"));
+    root.render(parent);
    
