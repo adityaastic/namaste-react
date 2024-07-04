e268821a9c24ebd88703d8dac17299f395241a56
adityaastic <adityaastic@gmail.com>
2024-07-04 19:03:31 +0530

    talking is cheap show me the code....

--- App.js
+++ App.js
@@ -1,75 +1,609 @@
 import { ClassNames } from "@emotion/react";
+import { Restaurant } from "@mui/icons-material";
 import { backdropClasses } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-const Header = ()=> {
-    return(
-        <div className="header">
-            <div className="logo-container">
-            <img className="logo"
-             src="https://t3.ftcdn.net/jpg/00/91/82/14/360_F_91821486_8MfLdTeRKvLdaDt7YVr1gJMnjvU1WiAU.jpg"/>
-            </div>
-            <div className="nav-items">
-             <ul>
-             <li>Home</li>
-             <li>About</li>
-             <li>Login</li>
-             <li>Cart</li>
-             </ul>
-            </div>
-        </div>
-    );
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://t3.ftcdn.net/jpg/00/91/82/14/360_F_91821486_8MfLdTeRKvLdaDt7YVr1gJMnjvU1WiAU.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Login</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 const styleCard = {
-    backgroundColor:"#f0f0f0",
-}
-
-
-const RestaurantCard =(props)=>{
-    return(
-        <div className="res-card" style={{backgroundColor :"#f0f0f0"}}>
-            <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/x4uyxvihmg8qa3pddkgf"/>
-            <h3>{props.resName}</h3>
-            <h4>{props.cuisins}</h4>
-            <h4> 4.3 star</h4>
-            <h4>39 mins</h4>
-
-        </div>
-    );
+  backgroundColor: "#f0f0f0",
 };
 
+const resList = [
+  {
+    info: {
+      id: "30531",
+      name: "Hotel Empire",
+      cloudinaryImageId: "qltgnkyywuo5gmnpqzbm",
+      locality: "Brigade Road",
+      areaName: "Church Street",
+      costForTwo: "₹450 for two",
+      cuisines: ["North Indian", "Kebabs", "Biryani"],
+      avgRating: 4.5,
+      parentId: "475",
+      avgRatingString: "4.5",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 34,
+        lastMileTravel: 2.2,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "2.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-07-05 03:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-North%20Indian.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-North%20Indian.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "UPTO ₹40",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-f20db07f-1fb3-4f59-8d4c-49ef65b65368",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/hotel-empire-brigade-road-church-street-bangalore-30531",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "30034",
+      name: "Mani's Dum Biryani",
+      cloudinaryImageId: "y0fjz8h4ad8nfzkmbznu",
+      locality: "Haudin Road",
+      areaName: "Ulsoor",
+      costForTwo: "₹499 for two",
+      cuisines: ["Andhra", "Biryani", "North Indian"],
+      avgRating: 4.5,
+      parentId: "623",
+      avgRatingString: "4.5",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 3.6,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "3.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-07-04 23:30:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-f20db07f-1fb3-4f59-8d4c-49ef65b65368",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/manis-dum-biryani-haudin-road-ulsoor-bangalore-30034",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "32738",
+      name: "Donne Biryani House",
+      cloudinaryImageId: "cgtfmodhd9gme13pblre",
+      locality: "Malleshwaram",
+      areaName: "Malleshwaram",
+      costForTwo: "₹300 for two",
+      cuisines: ["Biryani", "South Indian"],
+      avgRating: 4.2,
+      parentId: "338",
+      avgRatingString: "4.2",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 30,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-07-04 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "30% OFF",
+        subHeader: "UPTO ₹75",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-f20db07f-1fb3-4f59-8d4c-49ef65b65368",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/donne-biryani-house-malleshwaram-bangalore-32738",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "880372",
+      name: "Local Heros",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/28/e273ea47-3666-4a5c-950f-86cf8dba0119_880372.ss.jpg",
+      locality: "KR Garden",
+      areaName: "Koramangala",
+      costForTwo: "₹399 for two",
+      cuisines: ["Biryani", "Kebabs"],
+      avgRating: 4.9,
+      parentId: "516159",
+      avgRatingString: "4.9",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 34,
+        lastMileTravel: 6,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-07-04 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "30% OFF",
+        subHeader: "UPTO ₹75",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      isNewlyOnboarded: true,
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-f20db07f-1fb3-4f59-8d4c-49ef65b65368",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/local-heros-kr-garden-koramangala-bangalore-880372",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "210945",
+      name: "Royal Restaurant",
+      cloudinaryImageId: "yicweopn4lzcjlqy4jvq",
+      locality: "Shivaji Nagar",
+      areaName: "Shivaji Nagar",
+      costForTwo: "₹300 for two",
+      cuisines: ["Chinese", "Tandoor"],
+      avgRating: 4,
+      parentId: "2896",
+      avgRatingString: "4.0",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 29,
+        lastMileTravel: 2.6,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "2.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-07-11 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "30% OFF",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-f20db07f-1fb3-4f59-8d4c-49ef65b65368",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/royal-restaurant-shivaji-nagar-bangalore-210945",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "5938",
+      name: "Burger King",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/7f76a072-c1bc-4d74-ac56-33e0eea20c1e_5938.JPG",
+      locality: "Tasker Town",
+      areaName: "Shivaji Nagar",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.3,
+      parentId: "166",
+      avgRatingString: "4.3",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 2.6,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "2.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-07-05 03:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Burger.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Burger.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-f20db07f-1fb3-4f59-8d4c-49ef65b65368",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/burger-king-tasker-town-shivaji-nagar-bangalore-5938",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "510627",
+      name: "Lulu Hypermarket",
+      cloudinaryImageId: "usvu7jwfx9t47z3y3lkh",
+      locality: "Mysore Deviation Road",
+      areaName: "Rajajinagar",
+      costForTwo: "₹350 for two",
+      cuisines: ["Arabian", "Continental", "Chinese"],
+      avgRating: 4.2,
+      parentId: "19980",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 50,
+        lastMileTravel: 5.6,
+        serviceability: "SERVICEABLE",
+        slaString: "45-50 mins",
+        lastMileTravelString: "5.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-07-04 22:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "30% OFF",
+        subHeader: "UPTO ₹75",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-f20db07f-1fb3-4f59-8d4c-49ef65b65368",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/lulu-hypermarket-mysore-deviation-road-rajajinagar-bangalore-510627",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
 
-const Body = ()=>{
-    return (
-        <div className="body">
-            <div className="Search"> Search</div>
-            <div className="res-container">
-            <RestaurantCard resName="Magana Foods"
-            cuisins="Biryani,North,India Asian"
-            />
-            <RestaurantCard  resName="KFC"
-            cuisins="North,India Asian"/>
-            
+const RestaurantCard = ({ resData }) => {
+  const {
+    cloudinaryImageId,
+    deliveryTime,
+    costForTwo,
+    sla,
+    avgRating,
+    cuisines,
+    name,
+  } = resData?.info ;
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.deliveryTime}</h4>
+    </div>
+  );
+};
 
-            </div>      
-        </div>
-    )
-}
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search"> Search</div>
+      <div className="res-container">
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-const AppLayout = ()=>{
-    return(
-        <div className="app">
-            <Header/>
-            <Body/>
-          <body/>
-        </div>
-    )
-}
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <body />
+    </div>
+  );
+};
 
-    
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    
-    root.render(<AppLayout/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
